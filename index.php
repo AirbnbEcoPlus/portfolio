76a94050bc3cbe51f4393ec04326126627ebe2b1
AirbnbEcoPlus <adf7bn@gmail.com>
2022-09-02 18:25:52 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Adrien Faure</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+<h1>Adrien Faure</h1>
+<h2>Futur Developpeur BackEnd</h2>
+<h3>Qui suis-je ?</h3>
+<p>Bonjour je me presente, je m'appelle Adrien Faure, mais je suis plus connu sous le pseudonyme : AirbnbEcoPlus.</p>
+<p>Depuis ma plus tendre enfance j'ai bagné dans la technologie ce qui m'a permit de developper mes competences dans ce domaine.</p>
+<h3>Quelles sont mes motivation ?</h3>
+<p>AAAAh la programation, j'ai commencer a y porter un interer autour de l'age de 12 ans grace a Minecraft, depuis je n'arrete plus, j'ai cette envie de creer de nouveau moyen et d'améliorer mes competences<p>
+<h3>Ma Formation actuelle</h3>
+<p>Insert Frise Chronologique</p>
+<h3>Mes competences</h3>
+<iframe src="https://github-readme-stats.vercel.app/api/wakatime?username=todri1&langs_count=10" title="description" width="600" height="400"></iframe>
+<h3>Mes Projets</h3>
+<p>projects</p>
+<h4>Me contacter</h4>
+<script language="JavaScript">
+    TargetDate = "2028-09-01T00:00:00";
+    CountActive = true;
+    CountStepper = -1;
+    LeadingZero = true;
+    DisplayFormat = "%%D%% Days, %%H%% Hours, %%M%% Minutes, %%S%% Seconds.";
+    FinishMessage = "J'ai fini mes Etudes!";
+</script>
+<script language="JavaScript" src="https://rhashemian.github.io/js/countdown.js"></script>
+</body>
+</html>
