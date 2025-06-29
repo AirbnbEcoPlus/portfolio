e8e8b209eac0bd64842daa4df50b98467fb2fc74
AirbnbEcoPlus <adf7bn@gmail.com>
2025-06-29 15:21:16 +0200

    update cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adrienfaure.me',
+  site: 'https://adrienfaure.endide.com',
   integrations: [mdx(), sitemap(), icon()]
 });
